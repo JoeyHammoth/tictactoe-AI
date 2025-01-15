480a456c44eab6690a92f725ae3a4a389f978866
JoeyHammoth <137293223+JoeyHammoth@users.noreply.github.com>
2025-01-16 01:50:48 +0700

    Fixed replay button, add more choices to Master, allow for change in player order

--- Apps.py
+++ Apps.py
@@ -1,48 +1,84 @@
 import threading
 import tkinter as tk
-from tkinter import Button
+from tkinter import *
 from AppBoard import AppBoard
 from Master import Master
 
 # Global Stop Event 
 stop_event = threading.Event()
+player_first = False
+master = None
 
 def run_game(master, app):
     with master.condition:
-        while not stop_event.is_set() and master.game_board.check_board() == 0:
-            app.state = 1
-            app.move_done = False
-            while not stop_event.is_set() and app.move_done == False:
-                stop_event.wait(0.1)
-            if stop_event.is_set():
-                print("it is")
-                break
-            master.human_move = app.move
-            print(master.human_move)
-            master.condition.notify()
+        if player_first:
+            app.red = True
+            while not stop_event.is_set() and master.game_board.check_board() == 0:
+                app.state = 1
+                app.move_done = False
+                while not stop_event.is_set() and app.move_done == False:
+                    stop_event.wait(0.1)
+                if stop_event.is_set():
+                    break
+                master.human_move = app.move
+                master.condition.notify()
+                app.state = 0
+                master.condition.wait()
+                if stop_event.is_set():
+                    break
+                app.flip_board(master.opp_move, False)
+        else:
+            app.red = False
+            while not stop_event.is_set() and master.game_board.check_board() == 0:
+                master.condition.notify()
+                app.state = 0
+                master.condition.wait()
+                if stop_event.is_set():
+                    break
+                app.flip_board(master.opp_move, True)
+                if master.game_board.check_board() != 0:
+                    break
+                app.state = 1
+                app.move_done = False
+                while not stop_event.is_set() and app.move_done == False:
+                    stop_event.wait(0.1)
+                if stop_event.is_set():
+                    break
+                master.human_move = app.move
             app.state = 0
-            master.condition.wait()
-            if stop_event.is_set():
-                break
-            app.flip_board(master.opp_move, False)
 
 def start_game(master, app):
+    # Reset all the boards and restart threads
     app.hide_all()
     master.game_board.clear()
     stop_event.clear()
+
+    # daemon to keep track of threads
     threading.Thread(target=run_game, args=(master, app), daemon=True).start()
-    threading.Thread(target=master.run, daemon=True).start()
+    threading.Thread(target=master.run_human, daemon=True).start()
 
 def reset_game():
+    # Stop the current game (threads)
     stop_event.set()
-    print("helps")
     root.after(100, lambda: start_game(master, appBoard))
+
+def toggle_player_order():
+    global player_first, master
+    if player_first:
+        player_first = False
+        master = Master(type=3, ai_player=True)
+    else:
+        player_first = True
+        master = Master(type=3, ai_player=False)
+    # TKinter labels don't automatically reflect changes in external variables—they 
+    # need to be explicitly updated
+    order_label.config(text=f"Player is first: {player_first}") 
 
 root = tk.Tk()
 root.title("Tic-Tac-Toe")
 root.geometry("600x600")
 
-master = Master(type=3)
+master = Master(type=3, ai_player=True)
 appBoard = AppBoard(root)
 
 button_replay =  Button(root, text="Replay", width=25, command=lambda: reset_game())
@@ -51,7 +87,13 @@ button_replay.pack()
 button_clear = Button(root, text="Kill", width=25, command=appBoard.hide_all)
 button_clear.pack()
 
-# Start threads after tkinter is running
+order_label = Label(root, text=f"Player is first: {player_first}")
+order_label.pack()
+
+button_order = Button(root, text="Change Order", width=25, command=lambda: toggle_player_order())
+button_order.pack()
+
+# Initial game threads running
 root.after(100, start_game, master, appBoard)
 
 root.mainloop()
--- Master.py
+++ Master.py
@@ -9,23 +9,56 @@ import csv
 import threading
 
 class Master:
-    #red = Random()
-    blue = Random()
     game_board = Board()
 
-    def __init__(self, type=0, max=-1, c=-1, iterations=1000):
-        if type == 0:
-            self.blue = Mcts(True, c, iterations)
-        elif type == 1:
-            self.blue = Minimax(True, max)
-        elif type == 2:
-            self.blue = Nueral()
-        else:
+    def __init__(self, type=0, max=-1, c=-1, iterations=1000, ai_player=False, opp_type=0, opp_max=-1, opp_c=-1, opp_iterations=1000):
+        # If ai_player is False, Main Player is first, Opp is second
+        # Main Player
+        if type == 0: #MCTS
+            if ai_player:
+                self.blue = Mcts(True, c, iterations)
+            else:
+                self.red = Mcts(True, c, iterations)
+        elif type == 1: #MINIMAX
+            if ai_player:
+                self.blue = Minimax(True, max)
+            else:
+                self.red = Minimax(True, max)
+        elif type == 2: #MLP
+            if ai_player:
+                self.blue = Nueral()
+            else:
+                self.red = Nueral()
+        else: #HUMAN
             self.human_move = (-1,-1)
             self.condition = threading.Condition()
             self.opp_move = (-1,-1)
         
+        # Opponent
+        if opp_type == 0:
+            if ai_player:
+                self.red = Random()
+            else:
+                self.blue = Random()
+        elif opp_type == 1:
+            if ai_player:
+                self.red = Mcts(True, opp_c, opp_iterations)
+            else:
+                self.blue = Mcts(True, opp_c, opp_iterations)
+        elif opp_type == 2:
+            if ai_player:
+                self.red = Minimax(True, opp_max)
+            else:
+                self.blue = Minimax(True, opp_max)
+        else:
+            if ai_player:
+                self.red = Nueral()
+            else:
+                self.blue = Nueral()
+
         self.type = type
+        self.ai_player = ai_player
+        self.opp_type = opp_type
         
     def turn(self, player):
         valid = False
@@ -35,43 +68,74 @@ class Master:
             if (self.game_board.is_valid_move(answer[0], answer[1]) == True):
                 valid = True
         return answer
-
-    def run(self, filename=0):
+    
+    def run_human(self):
+        # Only use this if type == 3
         win = 0 
         self.game_board.clear()
         count = 0
         with self.condition:
-            while win == 0:
-                if self.type == 3:
-                    print("yes")
+            if not self.ai_player:
+                while win == 0:
                     if count > 0:
                         self.condition.wait()
                     self.game_board.flip(True, self.human_move[1], self.human_move[0])
                     self.game_board.print_board()
                     win = self.game_board.check_board()
-                else:
-                    red_answer = self.turn(self.red)
-                    self.game_board.flip(True, red_answer[0], red_answer[1])
-                    self.game_board.print_board()
-                    win = self.game_board.check_board()
 
-                if (win != 0):
-                    break
+                    if (win != 0):
+                        break
 
-                blue_answer = self.turn(self.blue)
-                if self.type == 3:
+                    blue_answer = self.turn(self.blue)
+
                     result = (blue_answer[1], blue_answer[0])
                     self.opp_move = result
                     self.condition.notify()
 
-                self.game_board.flip(False, blue_answer[0], blue_answer[1])
-                self.game_board.print_board()
-                win = self.game_board.check_board()
-                count += 1
+                    self.game_board.flip(False, blue_answer[0], blue_answer[1])
+                    self.game_board.print_board()
+                    win = self.game_board.check_board()
+                    count += 1
+            else:
+                while win == 0:
 
-        if self.type == 3:
-            with self.condition:
-                self.condition.notify()
+                    red_answer = self.turn(self.red)
+
+                    result = (red_answer[1], red_answer[0])
+                    self.opp_move = result
+                    self.condition.notify()
+
+                    self.game_board.flip(True, red_answer[0], red_answer[1])
+                    self.game_board.print_board()
+                    win = self.game_board.check_board()
+
+                    if (win != 0):
+                        break
+
+                    self.condition.wait()
+                    self.game_board.flip(False, self.human_move[1], self.human_move[0])
+                    self.game_board.print_board()
+                    win = self.game_board.check_board()
+
+            self.condition.notify()
+    
+
+    def run(self, filename=0):
+        win = 0 
+        self.game_board.clear()
+        while win == 0:
+            red_answer = self.turn(self.red)
+            self.game_board.flip(True, red_answer[0], red_answer[1])
+            self.game_board.print_board()
+            win = self.game_board.check_board()
+
+            if (win != 0):
+                break
+
+            blue_answer = self.turn(self.blue)
+            self.game_board.flip(False, blue_answer[0], blue_answer[1])
+            self.game_board.print_board()
+            win = self.game_board.check_board()
 
         if filename != 0:
             with open(filename, 'a', newline='') as file:
